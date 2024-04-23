6d1e458b7d0c165d6b0562e2ff73c0bb6a10017c
MeowRoar <sareacct91@gmail.com>
2024-04-22 23:58:06 -0700

    client proxy '/externalapi/photos' to server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.PORT;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      'externalapi/photos': {
+        target: `http://localhost:3001`,
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })

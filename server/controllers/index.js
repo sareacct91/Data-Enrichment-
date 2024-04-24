8386da3db0d7e319de9fce91620cfe715292e897
MeowRoar <sareacct91@gmail.com>
2024-04-23 17:55:05 -0700

    fix(400 error): Change parsInt to unary plus operator
    parsInt doesn't catch '111ddd'

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -8,24 +8,31 @@ const USERS_URL = 'https://jsonplaceholder.typicode.com/users/';
 
 module.exports = {
   async getPhotos(req, res, next) {
+    const queryParams = req.query;
+    console.log(req.query)
 
 
-    res.status(200).json({msg: 'success'}); 
+
+    res.status(200).json({msg: `success ${queryParams}`}); 
   },
 
   async getOnePhoto(req, res, next) {
     const { photoId } = req.params;
 
     try {
-      if (isNaN(parseInt(photoId))) {
+      if (isNaN(+photoId)) {
         throw new BadRequestError(`${photoId} is not a valid number`);
       }
 
-      const p1 = queryInteralEndpoint(PHOTOS_URL + photoId);
-      const p2 = queryInteralEndpoint(ALBUMS_URL);
-      const p3 = queryInteralEndpoint(USERS_URL);
-
-      const [photo, albumData, userData] = await Promise.all([p1, p2, p3]);
+      // const p1 = queryInteralEndpoint(PHOTOS_URL + photoId);
+      // const p2 = queryInteralEndpoint(ALBUMS_URL);
+      // const p3 = queryInteralEndpoint(USERS_URL);
+      //
+      // const [photo, albumData, userData] = await Promise.all([p1, p2, p3]);
+      // const album = albumData.find(e => e.id === photo.albumId);
+      // const user = userData.find(e => e.id === album.userId);
+      
+      const [photo, albumData, userData] = await queryInteralEndpoint(photoId);
       const album = albumData.find(e => e.id === photo.albumId);
       const user = userData.find(e => e.id === album.userId);
 

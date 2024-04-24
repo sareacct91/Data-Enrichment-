f50d5f3062db097bf10432362556de34e462d625
MeowRoar <sareacct91@gmail.com>
2024-04-23 17:47:45 -0700

    refactor(fetch): fetch all 3 endpoints in helper function
    Instead of fetching 1 endpoint in the helper function
    and calling it 3 times in the controller.
    Change to calling all 3 in helper function and calling
    helper function 1 time.

--- server/utils/API.js
+++ server/utils/API.js
@@ -1,14 +1,28 @@
-module.exports = {
-  async queryInteralEndpoint(URL) {
-    const response = await fetch(URL);
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos/';
+const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums/';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users/';
 
-    if (!response.ok) {
-      throw {
-        code: response.status,
-        message: response.statusText
-      } 
+module.exports = {
+  async queryInteralEndpoint(id) {
+    const promisePhotos = fetch(PHOTOS_URL + id);
+    const promiseAlbums = fetch(ALBUMS_URL);
+    const promiseUsers = fetch(USERS_URL);
+
+    const responseArr = await Promise.all(
+          [promisePhotos, promiseAlbums, promiseUsers]
+    );
+
+    const dataArr = [];
+    for (const response of responseArr) {
+      if (!response.ok) {
+        throw {
+          code: response.status,
+          message: response.statusText 
+        }
+      }
+      dataArr.push(response.json());
     }
 
-    return await response.json();
+    return await Promise.all(dataArr);
   },
 };

cc1efc48321d5c680a9f187853d0502f12ff55dd
MeowRoar <sareacct91@gmail.com>
2024-04-25 00:49:34 -0700

    refactor(init): inital client setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const PORT = process.env.PORT;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],

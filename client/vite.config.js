669404f67309b7c10c53d686be1d72e9da1c322b
MeowRoar <sareacct91@gmail.com>
2024-04-26 21:03:41 -0700

    fix(proxy): added missing '/' in front of route

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      'externalapi/photos': {
+      '/externalapi/photos': {
         target: `http://localhost:3001`,
         changeOrigin: true,
         secure: false,
